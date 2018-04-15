2a3fb8fa43b593464554c09076a4f3315308ae05
Stevanus Surjadi <stevanussurjadi@Stevanuss-MacBook-Pro.local>
2018-04-15 20:48:42 +0700

    Add Source Code Folder and initial DB PHP Class

---
+++ sourcecode/php/class/db.mysql.class.php
@@ -0,0 +1,34 @@
+<?php
+
+class database
+{
+    private $dbconn;
+    private $dbhost;
+    private $dbuser;
+    private $dbpass;
+    private $dbname;
+
+    private function setDBhost($host)
+    {
+        $this->$dbconn = $arg1;
+    }
+
+    private function setDBcredential($user, $pass)
+    {
+        $this->$dbuser = $user;
+        $this->$dbpass = $pass;
+    }
+
+    private function setDBname($name)
+    {
+        $this->$dbname = $name;
+    }
+
+    public function connectDB()
+    {
+
+
+    }
+}
+
+?>
